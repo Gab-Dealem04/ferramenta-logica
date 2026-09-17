7223603399d0b09ccdecb8c8c890cb70498e1aae
Gab-Dealem04 <gabrielycorreia@alu.ufc.br>
2026-09-17 10:39:11 -0300

    fix: corrige base do vite para o nome correto do repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/ferramenta-logica-two/' : '/',
+  base: command === 'build' ? '/ferramenta-logica/' : '/',
   server: {
     host: true,
     port: 5173,
